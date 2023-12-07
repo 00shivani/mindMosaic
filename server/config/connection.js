6b928bd9e06f886161e3af5193034c6bdcca7e03
00shivani <shivanisingh13@icloud.com>
2023-12-07 16:52:18 -0500

    edits to connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,9 +2,8 @@ const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/');
 
-
-Db.on('error', console.error.bind(console, 'mongodb connection error:'));
-Db.once('open', function() {
+db.on('error', console.error.bind(console, 'mongodb connection error:'));
+db.once('open', function() {
   console.log("connected to the database!")
 });
 
