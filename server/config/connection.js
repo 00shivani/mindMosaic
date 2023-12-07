2aebd40612c41a58648cedf9f330089df8246a81
jimbuckley412 <jimbuckley412@gmail.com>
2023-12-07 18:17:19 -0500

    serverside

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,14 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/mindMosaic');
 
+const db = mongoose.connection;
+
+// Move the event listeners here
 db.on('error', console.error.bind(console, 'mongodb connection error:'));
-db.once('open', function() {
-  console.log("connected to the database!")
+db.once('open', function () {
+  console.log('connected to the database!');
 });
 
-module.exports = mongoose.connection;
+module.exports = db;
