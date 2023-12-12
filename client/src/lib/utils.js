3752abbdaf5799efd20daa676726f2756ca225df
00shivani <shivanisingh13@icloud.com>
2023-12-11 19:18:57 -0500

    receiving values from form, reverted some changes

--- client/src/lib/utils.js
+++ client/src/lib/utils.js
@@ -1,6 +1,10 @@
-import { clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
 export function cn(...inputs) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(...inputs));
 }
+
+// // Example usage:
+// const result = cn("class1", "class2");
+// console.log(result);
