8b88383a08f79d098b55dfad53f62d74d1f8ac49
00shivani <shivanisingh13@icloud.com>
2023-12-11 20:51:50 -0500

    cleaned up files, merged components from different branches

--- client/src/components/ui/Sidebar.jsx
+++ client/src/components/ui/Sidebar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom'; 
-import './Sidebar.css';
+import '../../css/Sidebar.css';
 
 const Sidebar = () => {
   const [isExpanded, setIsExpanded] = useState(false);
